46d1d6feffd30e5173a48b02195115b981fc4dde
alan <alanarostegui@hotmail.com>
2022-05-07 02:05:09 -0500

    feat: add nextUI React Library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import { Button } from "@nextui-org/react";
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Hey</h1>
+      <Button color="gradient">Hola mundo</Button>
     </>
   );
 };

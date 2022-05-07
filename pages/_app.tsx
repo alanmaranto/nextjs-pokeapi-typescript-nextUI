1194c8f976a9a49974e5e9f21ef01be69def580f
alan <alanarostegui@hotmail.com>
2022-05-07 02:21:44 -0500

    feat: add dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import { darkTheme } from "../themes";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
   );

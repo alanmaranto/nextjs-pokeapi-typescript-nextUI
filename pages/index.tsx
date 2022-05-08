3a8a4459f3802768a481803f8f74c6a2e616fdcd
alan <alanarostegui@hotmail.com>
2022-05-08 01:11:49 -0500

    enhacement: show name and id

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,22 @@ import type { NextPage, GetStaticProps } from "next";
 import { Button } from "@nextui-org/react";
 import { Layout } from "../components/layouts";
 import { pokeApi } from "../api";
-import { PokemonListResponse } from "../interfaces";
+import { PokemonListResponse, SmallPokemon } from "../interfaces";
 
-const Home: NextPage = (props) => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title="List Items pokemon">
-      <h1>Hey</h1>
-      <Button color="gradient">Hola mundo</Button>
+      <ul>
+        {pokemons.map(({ id, name }) => (
+          <li key={name}>
+            #{id} - {name}
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 };
@@ -19,9 +28,17 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
   // quicktype.io
 
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => ({
+    ...pokemon,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      i + 1
+    }.png`,
+  }));
+
   return {
     props: {
-      pokemons: data.results,
+      pokemons,
     },
   };
 };

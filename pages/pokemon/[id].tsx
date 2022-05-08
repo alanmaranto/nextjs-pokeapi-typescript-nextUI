6d9ef511cb00338463cf71ac3b631ef8b6a5241e
alan <alanarostegui@hotmail.com>
2022-05-08 01:51:20 -0500

    feat: get static paths for dynamic pages url

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,18 +1,49 @@
+import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/router";
 import { Layout } from "../../components/layouts";
 
 interface Props {
-  pokemon: any;
+  //   pokemon: any;
+  id: string;
+  name: string;
 }
 
-const Pokemon = () => {
+const Pokemon: NextPage<Props> = ({ id, name }) => {
   const router = useRouter();
 
   return (
     <Layout title="Pokemon">
-      <h1>Pokemon</h1>
+      <h1>
+        {id} - {name}{" "}
+      </h1>
     </Layout>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  // const { data } = await  // your fetch function here
+
+  return {
+    paths: [
+      {
+        params: {
+          id: "1",
+        },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  // const { data } = await  // your fetch function here
+
+  return {
+    props: {
+      id: 1,
+      name: "Bulbasaur",
+    },
+  };
 };
 
 export default Pokemon;

14f3a0cbfb0e9dbead4303efe3b5f2f789d7b243
alan <alanarostegui@hotmail.com>
2022-05-08 15:51:24 -0500

    feat: nav return home and add favorite page

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import { Spacer, Text, useTheme, Link } from "@nextui-org/react";
 import Image from "next/image";
+import NextLink from "next/link";
 import React from "react";
 
 export const Navbar = () => {
@@ -21,14 +22,22 @@ export const Navbar = () => {
         width={70}
         height={70}
       />
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okémon
-      </Text>
+      <NextLink href="/" passHref>
+        <Link>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okémon
+          </Text>
+        </Link>
+      </NextLink>
       <Spacer css={{ flex: 1 }} />
-      <Text color="white">Favoritos</Text>
+      <NextLink href="/favorites" passHref>
+        <Link>
+          <Text color="white">Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
   );
 };
---
+++ pages/favorites/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from '../../components/layouts';
+
+const Favorites = () => {
+  return (
+    <Layout title="Favorites Pokemon">
+      <div>Favorites</div>
+    </Layout>
+  );
+};
+
+export default Favorites;

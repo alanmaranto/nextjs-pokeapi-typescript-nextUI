9ee34f1b6c25b0c5bf9cea0fdeec803eef207637
alan <alanarostegui@hotmail.com>
2022-05-08 22:04:46 -0500

    feat: set pokemon items in favorite page

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,3 +1,4 @@
+import { Card, Grid } from "@nextui-org/react";
 import React, { useEffect, useState } from "react";
 import { Layout } from "../../components/layouts";
 import { NoFavorites } from "../../components/ui";
@@ -12,7 +13,23 @@ const Favorites = () => {
 
   return (
     <Layout title="Favorites Pokemon">
-      <NoFavorites />
+      {favoritePokemons.length > 0 ? (
+        <Grid.Container gap={2} direction="row" justify="flex-start">
+          {favoritePokemons.map((pokemonId) => (
+            <Grid xs={6} sm={3} md={2} lg={1} key={pokemonId}>
+              <Card hoverable clickable css={{ padding: 10 }}>
+                <Card.Image
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
+                  width="100%"
+                  height={140}
+                />
+              </Card>
+            </Grid>
+          ))}
+        </Grid.Container>
+      ) : (
+        <NoFavorites />
+      )}
     </Layout>
   );
 };

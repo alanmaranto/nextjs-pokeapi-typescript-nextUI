97103c39c2809e0dd04df8b74dfb302b3544169b
alan <alanarostegui@hotmail.com>
2022-05-12 21:58:27 -0500

    feat: add open graph meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,7 +6,10 @@ interface Props {
   title?: string;
 }
 
+const origin = (typeof window === 'undefined' ? '' : window.location)
+
 export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
+
   return (
     <>
       <Head>
@@ -14,9 +17,24 @@ export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
         <meta name="author" content="Alan Maranto"></meta>
         <meta name="description" content={`Info about pokemon ${title}`}></meta>
         <meta name="keywords" content={`${title}, pokemon, pokedex`}></meta>
+
+        <meta
+          property="og:title"
+          content={`Pokemon data about ${title}`}
+        />
+        <meta
+          property="og:description"
+          content={`This page is about ${title}`}
+        />
+        <meta
+          property="og:image"
+          content={`${origin}/img/banner.png`}
+        />
       </Head>
       <Navbar />
       <main style={{ padding: "opx 20px" }}>{children}</main>
     </>
   );
 };
+
+// https://ahrefs.com/blog/open-graph-meta-tags/
